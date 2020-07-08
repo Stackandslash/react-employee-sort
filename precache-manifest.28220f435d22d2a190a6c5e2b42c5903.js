0dd9af8d4e4a01dab89d8d0b4b02793180c5f954
Haven <stackandslash@gmail.com>
2020-07-08 14:53:53 -0700

    Updates

---
+++ precache-manifest.28220f435d22d2a190a6c5e2b42c5903.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "409e06757a8949c98f3cbab55b0c247c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31731d1e0826e6a54b5b",
+    "url": "/static/css/main.7aa77705.chunk.css"
+  },
+  {
+    "revision": "af2414fff2bc0b37b01f",
+    "url": "/static/js/2.63f2ba8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.63f2ba8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31731d1e0826e6a54b5b",
+    "url": "/static/js/main.7f0018af.chunk.js"
+  },
+  {
+    "revision": "cd621cfb40d2deafc82d",
+    "url": "/static/js/runtime-main.07b7731d.js"
+  }
+]);
